ae4882e0a9073b294e74859ea78eff35c2919830
Kazi Mejbaul Islam <222mzb@gmail.com>
2017-02-02 21:52:56 +0600

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,12 +2,14 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 from PIL import Image
+import os
 
 temp = plt.imread('1.jpg')
 i_h, i_w, c= temp.shape
 temp = temp[0:i_h, 10:i_w]
 h,w, c= temp.shape
 
+location  = os.getcwd()
   
 
 x = 0
@@ -23,16 +25,21 @@ else:
     while y<h:    
 
         while x<= w:
-            img_1 = temp[y :part_y, x:part_x]
-            x = part_x
-            part_x = part_x + part_x
-            
-            plt.imshow(img_1, cmap = 'gray')
-            plt.show()
-            plt.imsave('img' + str(n), img_1)
-            n = n+1
-            if n==9:
-                n=5
+            if(os.path.exists('photos')):
+                os.chdir(location + '\\photos')
+                img_1 = temp[y :part_y, x:part_x]
+                x = part_x
+                part_x = part_x + part_x
+                
+                plt.imshow(img_1, cmap = 'gray')
+                plt.show()
+                plt.imsave('img' + str(n), img_1)
+                n = n+1
+                if n==9:
+                    n=5
+            else:
+                os.mkdir('photos')
+                continue
 
         x = 0    
         part_x = w/3
