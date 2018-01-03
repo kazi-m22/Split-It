a7e780151269f1f72d9761c9e2f69bef359c0102
misbah <222mzb@gmail.com>
2018-01-04 02:46:29 +0600

    threshold 2:11

--- crop.py
+++ crop.py
@@ -14,10 +14,39 @@ for t in images:
 images = im
 print(images)
 i=0
-while i<len(images):
+# while i<len(images):
+#
+#     img = cv2.imread(images[i])
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+#     cv2.imwrite(images[i], img)
+#     i = i + 1
 
-    img = cv2.imread(images[i])
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(images[i], img)
-    i = i + 1
+
+for im in images:
+    img = cv2.imread(im)
+    h,w,c = img.shape
+    img = img[200:h, 50:w]
+    h,w,c = img.shape
+    x = 0
+    y = 0
+    part_y = y/4
+    part_x = w/4
+
+    while y<h:
+        while x<w:
+
+            img_1 = img[int(y):int(part_y), int(x): int(part_x)]
+            x = part_x
+            part_x = part_x + part_x
+            if os.path.exists('photos'):
+                cv2.imwrite('./photos/%d'%n+'.jpg', img_1)
+            else:
+                os.mkdir('photos')
+                cv2.imwrite('./photos/%d'%n + '.jpg', img_1)
+            n += 1
+        x = 0
+        part_x = w / 3
+        y = part_y
+        part_y = part_y + (h / 4)
+
