6a524b8e76b9794ddc52ca04013cee8ad2853ee0
misbah <222mzb@gmail.com>
2018-01-04 02:11:41 +0600

    threshold 2:11

--- crop.py
+++ crop.py
@@ -5,18 +5,19 @@ import numpy as np
 
 
 base = os.getcwd()
-im_loc = base + '\\images'
-images = os.listdir(base + '\\images')
+im_loc = './images'
+images = os.listdir(im_loc)
 im = []
 n = 1
-for i in images:
-    im.append(im_loc+'\\'+i)
+for t in images:
+    im.append(im_loc+'/'+t)
 images = im
 print(images)
-for img in images:
-    i = cv2.imread(img,0)
-    # i = cv2.cvtColor(i, cv2.COLOR_BAYER_BG2GRAY)
-    # ret, i = cv2.threshold(i, 180, 255, cv2.THRESH_BINARY)
-    cv2.imwrite('%d'%n+'.jpg', i)
-    n+=1
+i=0
+while i<len(images):
 
+    img = cv2.imread(images[i])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(images[i], img)
+    i = i + 1
--- foldertresh.py
+++ foldertresh.py
@@ -4,7 +4,7 @@ import numpy as np
 
 print(os.getcwd())
 
-location = "E:\\Projects\\python\\shahidul dataset\\2"  #path of that folder which contains pictures
+location = "C:\\Users\\misbah\\PycharmProjects\\Split-It\\images"  #path of that folder which contains pictures
 os.chdir(location)
 
 images = os.listdir()
@@ -15,7 +15,7 @@ i = 0
 while(i<length):
     img = cv2.imread(images[i])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, img = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
+    ret, img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
     cv2.imwrite(images[i], img)
     i = i+1
 
